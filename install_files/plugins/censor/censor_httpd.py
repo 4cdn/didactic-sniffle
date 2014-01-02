5e19655bf2565901440c4d31b21672aac9f40e31
SeekingFor <SeekingFor@i2pmail.org>
2014-01-02 05:06:10 +0000

    moderation: refresh session timeout on request

--- install_files/plugins/censor/censor_httpd.py
+++ install_files/plugins/censor/censor_httpd.py
@@ -39,7 +39,7 @@ class censor(BaseHTTPRequestHandler):
       public = self.check_login() 
       if public:
         session = hexlify(self.origin.rnd.read(24))
-        self.origin.sessions[session] = (int(time.time()) + 3600, public)
+        self.origin.sessions[session] = [int(time.time()) + 3600, public]
         self.send_redirect('/moderate/%s/' % session, 'access granted. this time.')
       else:
         self.send_login("totally not")
@@ -51,6 +51,7 @@ class censor(BaseHTTPRequestHandler):
       if self.origin.sessions[self.path[10:58]][0] < int(time.time()):
         self.send_login()
         return
+      self.origin.sessions[self.path[10:58]][0] = int(time.time()) + 3600
       path = self.path[58:]
       if path.startswith('/modify?'):
         key = path[8:]
@@ -89,6 +90,7 @@ class censor(BaseHTTPRequestHandler):
       if self.origin.sessions[self.path[10:58]][0] < int(time.time()):
         self.send_login()
         return
+      self.origin.sessions[self.path[10:58]][0] = int(time.time()) + 3600
       path = self.path[58:]
       if path.startswith('/modify?'):
         key = path[8:]
