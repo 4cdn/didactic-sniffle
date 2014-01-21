46440f3cc77fe0ec899f4873c2f0584d54a86579
SeekingFor <SeekingFor@i2pmail.org>
2014-01-21 04:23:45 +0000

    detect *BSD and use kqueue() instead of F_NOTIFY to get notifications about new files in incoming/

--- main.pyx
+++ main.pyx
@@ -1,27 +1,50 @@
 #!/usr/bin/python
 import time
 import signal
-import fcntl
 import os
 import threading
+import platform
 
 import SRNd
 
+if platform.system().lower() == 'linux':
+  print "[SRNd] linux detected, using F_NOTIFY"
+  import fcntl
+  bsd = False
+elif platform.system().lower().endswith('bsd'):
+  print "[SRNd] *BSD detected: %s, using select.kqueue()" % platform.system()
+  import select
+  try:
+    queue = select.kqueue()
+    bsd = True
+  except Exception as e:
+    print "[SRNd] could not load BSD kqueue: %s" % e
+    exit(1)
+else:
+  print "[SRNd] unsupported platform: '%s'" % platform.system()
+  exit(1)
+
 srnd = SRNd.SRNd()
-#signal.signal(signal.SIGIO, srnd.relay_dropper_handler)
-signal.signal(signal.SIGIO, srnd.dropper.handler_progress_incoming)
+fd = os.open(srnd.watching(), os.O_RDONLY | os.O_NONBLOCK)
+
+if bsd:
+  watching = (select.kevent(fd, filter=select.KQ_FILTER_VNODE, flags=select.KQ_EV_ADD | select.KQ_EV_ENABLE | select.KQ_EV_CLEAR, fflags=select.KQ_NOTE_WRITE),)
+else:
+  signal.signal(signal.SIGIO, srnd.dropper.handler_progress_incoming)
+  fcntl.fcntl(fd, fcntl.F_SETSIG, 0)
+  fcntl.fcntl(fd, fcntl.F_NOTIFY, fcntl.DN_MODIFY | fcntl.DN_CREATE | fcntl.DN_MULTISHOT)
+
 signal.signal(signal.SIGHUP, srnd.update_hooks_outfeeds_plugins)
-fd = os.open(srnd.watching(), os.O_RDONLY)
-fcntl.fcntl(fd, fcntl.F_SETSIG, 0)
-fcntl.fcntl(fd, fcntl.F_NOTIFY,
-            fcntl.DN_MODIFY | fcntl.DN_CREATE | fcntl.DN_MULTISHOT)
 srnd.start()
-#srnd.dropper.start()
-#time.sleep(1)
-#srnd.dropper.handler_progress_incoming(None, None)
+# TODO initialize with calling srnd.dropper.hanler_progress_incoming(None, None), best suited in SRNd run() itself.
 while True:
   try:
-    time.sleep(3600)
+    if bsd:
+      print "[SRNd] reading events.."
+      print "[SRNd] got events: '%s'" % str(queue.control(watching, 1, None))
+      srnd.dropper.handler_progress_incoming(None, None)
+    else:
+      time.sleep(3600)
   except KeyboardInterrupt:
     print
     print "[SRNd] shutting down.."
