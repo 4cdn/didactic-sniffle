bd7fe6aaa3d6418cd0261c0eeace372bacfab57a
Hidden Z <hiddenz@mail.i2p>
2014-11-23 13:52:01 +0000

    overchan: fix XSS in url parser

--- install_files/plugins/overchan/overchan.py
+++ install_files/plugins/overchan/overchan.py
@@ -790,7 +790,7 @@ class main(threading.Thread):
     # make >quotes
     quoter = re.compile("^&gt;(?!&gt;[0-9a-f]{10}).*", re.MULTILINE)
     # Make http:// urls in posts clickable
-    clicker = re.compile("(http://|https://|ftp://|mailto:|news:|irc:)([^(\s\[\]<>)]*)")
+    clicker = re.compile("(http://|https://|ftp://|mailto:|news:|irc:)([^(\s\[\]<>'\"&)]*)")
     # make code blocks
     coder = re.compile('\[code](?!\[/code])(.+?)\[/code]', re.DOTALL)
     # make spoilers
